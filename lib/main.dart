3dded32b174509f6897401d7492ee9c7a3af39d0
Saja Ramzi <sajaramzi99@gmail.com>
2021-11-23 13:09:44 +0200

    saja updatet

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
-      title: 'lama ramzi meri',
+      title: 'lama ramzi meri saja ',
       theme: ThemeData(
         // This is the theme of your application.
         //
